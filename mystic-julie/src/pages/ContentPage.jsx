ea2fc019265fe611468ccb3b99c16381420340e9
Nick McCullough <nick@Nicks-Mac-mini.local>
2026-07-26 06:06:22 -0500

    testing content page on vercel

--- mystic-julie/src/pages/ContentPage.jsx
+++ mystic-julie/src/pages/ContentPage.jsx
@@ -4,7 +4,7 @@ const trailerVideo = {
   title: 'Channel Trailer',
   url: 'https://www.twitch.tv/videos/2629823914',
   embedUrl:
-    'https://player.twitch.tv/?video=2629823914&parent=localhost&autoplay=false',
+    'https://player.twitch.tv/?video=2629823914&parent=localhost&parent=mysticjulie.com&parent=www.mysticjulie.com&autoplay=false',
   description:
     'Check out my Twitch channel trailer!',
 }
@@ -12,29 +12,27 @@ const trailerVideo = {
 const highlightClip = {
     title: 'IRL Picnic :DD',
     embedUrl:
-      'https://clips.twitch.tv/embed?clip=HedonisticEnticingOwlDxAbomb-s--cBj6OeBkB6bxL&parent=localhost',
+      'https://clips.twitch.tv/embed?clip=HedonisticEnticingOwlDxAbomb-s--cBj6OeBkB6bxL&parent=localhost&parent=mysticjulie.com&parent=www.mysticjulie.com',
     description: 'Funny clip of this dog eating our food!! hehe',
 }
 
 const clipVideos = [
   {
-
-
       title: 'Valorant Ace Clip',
   embedUrl:
-    'https://clips.twitch.tv/embed?clip=MiniatureCreativeGoblinStoneLightning-wMSkUbFUcS7YSooQ&parent=localhost',
+    'https://clips.twitch.tv/embed?clip=MiniatureCreativeGoblinStoneLightning-wMSkUbFUcS7YSooQ&parent=localhost&parent=mysticjulie.com&parent=www.mysticjulie.com',
   description: 'Check out my ace during a tournament scrim :o',
   },
   {
     title: 'Funny IRL Stream Moment during a 24hr Stream with Alisa_Veez!',
     embedUrl:
-      'https://clips.twitch.tv/embed?clip=GenerousVibrantKangarooEleGiggle-q2FOJxrRJjyQdaVC&parent=localhost',
+      'https://clips.twitch.tv/embed?clip=GenerousVibrantKangarooEleGiggle-q2FOJxrRJjyQdaVC&parent=localhost&parent=mysticjulie.com&parent=www.mysticjulie.com',
     description: 'LOL',
   },
   {
     title: 'A nasty clutch on Valorant :DD',
     embedUrl:
-      'https://clips.twitch.tv/embed?clip=LovelyVenomousLeopardShadyLulu-W_CQY0i6jAcXUFvF&parent=localhost',
+      'https://clips.twitch.tv/embed?clip=LovelyVenomousLeopardShadyLulu-W_CQY0i6jAcXUFvF&parent=localhost&parent=mysticjulie.com&parent=www.mysticjulie.com',
       description: 'WOOOO',
   }
 ]
